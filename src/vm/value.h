e4370ebe9ba8279c3b6283ee4b3a8e383bb778d5
Linus Aronsson <linaro@localhost.localdomain>
2018-08-11 17:04:27 +0200

    value progress

--- src/vm/value.h
+++ src/vm/value.h
@@ -2,11 +2,12 @@
 #define VALUE_H
 
 #include <cstddef>
-
+#include <memory>
 namespace linaro {
 
 enum ValueType {
     NUMBER,
+    BOOLEAN,
     STRING,
     FUNCTION,
     ARRAY,
@@ -15,14 +16,27 @@ enum ValueType {
     NOLL
 };
 
+class Function;
+
 /*
  * Value represents any data type (dynamically typed).
  * Changes todo: Value cant be abstract if used in unorderd map. 
  */
 class Value {
 public:
+    Value() {}
+    virtual ~Value() {}
+    
+    // Value interface
     virtual void print() const = 0;
     virtual size_t hash() const = 0;
+ 
+    // Value helper methods (overridden if they can be applied)
+    virtual  bool canBeNumber() const { return false; }
+    virtual double asNumber() const { return 0; }
+    virtual bool asBoolean() const { return false; }
+    virtual std::string asString() const { return "undefined"; }
+    virtual std::shared_ptr<Function> asFunction() const { return nullptr; }
 
     // Required for c++ hash map.
     virtual bool operator==(const Value& lhs) = 0;
@@ -31,15 +45,24 @@ public:
             return v.hash(); //defined in each value type class
         }
     }; 
-
     
+    // Value helper methods
 
-private:
+protected:
     ValueType m_type;
-    double m_value;
 };
 
-class Number : public Value {};
+class Number : public Value { 
+public:
+    explicit Number();
+    void print() const override;    
+    size_t hash() const override;
+    
+private:
+    double m_val;
+};
+
+
 class String : public Value {};
 class Function : public Value {};
 class Array : public Value {};
